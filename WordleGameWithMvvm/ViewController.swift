b5ce3a20e9351554fb2fb9542ec30cb4a56b1002
Mustafa Çiçek <f191220093@ktun.edu.tr>
2022-08-13 16:06:52 +0300

    Initial Commit

---
+++ WordleGameWithMvvm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WordleGameWithMvvm
+//
+//  Created by Mustafa Çiçek on 13.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
